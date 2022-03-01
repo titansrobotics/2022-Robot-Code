76b58af14463839fd3d6e7e56fc23d64a13979a1
titansrobotics <titanrobotics8174@gmail.com>
2022-03-01 15:31:05 -0700

    Drivetrain Basics

--- robot.py
+++ robot.py
@@ -1,13 +1,14 @@
 import wpilib
 from wpilib.drive import DifferentialDrive
+from constants import LEFT_MOTOR_ID, RIGHT_MOTOR_ID
 
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self):
-        # left_motor = wpilib.
-        # right_monitor = wpilib.
-        # self.drive = DifferentialDrive()
-        print('Code is running...')
+        left_motor = wpilib.PWMVictorSPX(LEFT_MOTOR_ID)
+        right_motor = wpilib.PWMVictorSPX(RIGHT_MOTOR_ID)
+        self.drive = DifferentialDrive(left_motor, right_motor)
+        print('Robot initialized...')
 
 
 if __name__ == '__main__':
